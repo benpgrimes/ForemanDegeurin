6e8cdb04aab64e279099dfeb4a5763d047922ad6
benpgrimes <bengrimes139@gmail.com>
2023-02-25 18:07:05 -0600

    remove andres

--- src/app/attorneys/attorneys.component.ts
+++ src/app/attorneys/attorneys.component.ts
@@ -23,10 +23,7 @@ export class AttorneysComponent {
     this.matDialog.open(this.MichaelDialogTemplateRef);
   }
 
-  openAndresDialog() {
-    this.matDialog.open(this.AndresDialogTemplateRef);
-  }
-
+  
   openBryanDialog() {
     this.matDialog.open(this.BryanDialogTemplateRef);
   }
